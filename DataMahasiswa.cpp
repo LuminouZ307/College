3a76898eb3a405965af87a4fb9ae89f35b5f5dee
LuminouZ307 <rifrh775@gmail.com>
2025-02-16 12:52:14 +0700

    first commit

---
+++ DataMahasiswa.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    std::cout << "============= Data Mahasiswa =============\n";
+    std::cout << "| Nama   :\tArif Rahman Hudaya\t|\n";
+    std::cout << "| NPM    :\t2420501046\t\t|\n";
+    std::cout << "| Asal   :\tBogor\t\t\t|\n";
+    std::cout << "-------------- Mata Kuliah ---------------\n";
+    std::cout << "| Kode\t| Nama Matkul\t\t  | SKS |\n";
+    std::cout << "------------------------------------------\n";
+    std::cout << "| MK001\t| Matematika\t\t  |  3  |\n";
+    std::cout << "| MK002\t| Basis Data\t\t  |  2  |\n";
+    std::cout << "| MK003\t| Data Mining\t\t  |  3  |\n";
+    std::cout << "------------------------------------------\n";
+    std::cout << "| Total SKS\t\t\t  |  8  |\n";
+    std::cout << "==========================================\n";
+    
+    return 0;
+}
---
+++ HelloWorld2.cpp
@@ -0,0 +1,19 @@
+/* --------------------------------------------
+   Nama Program  : Hello World 2
+   Nama          : Arif Rahman Hudaya
+   NPM           : 2420501046
+   Tanggal Buat  : 16 Februari 202
+   Deskripsi     : Menampilkan Output Hello World
+   -------------------------------------------- */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Menampilkan output
+    cout << "Hello World!!! \n";
+    cout << "\nBelajar Bahasa C++" << endl;
+    cout << "Teknik Elektro Untidar";
+    
+    return 0;
+}

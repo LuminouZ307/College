437d7c0156152704ffc2c9df4839bc2068b08ef2
LuminouZ307 <rifrh775@gmail.com>
2025-02-24 12:00:50 +0700

    fix:

--- Modul_3/Modul3_Evaluasi.cpp
+++ Modul_3/Modul3_Evaluasi.cpp
@@ -1,58 +1,48 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main() {
-    int jam;
-    double totalBiaya = 0;
-    double HARGA_24_JAM_PERTAMA = 455000;
-    double HARGA_2_JAM_KEDUA = 55000;
-    double HARGA_2_JAM_KETIGA = 75000;
-    double HARGA_2_JAM_SELANJUTNYA = 95000;
-    double DISKON_35_JAM = 0.15;
-    double DISKON_48_JAM = 0.10;
+    int pemakaian;
+    double totalBiaya = 0.0;
 
-    cout << "Masukkan lama pemakaian (jam): ";
-    cin >> jam;
+    cout << "Lama pemakaian: ";
+    cin >> pemakaian;
 
-    if (jam <= 24) {
-        totalBiaya = HARGA_24_JAM_PERTAMA;
+    if (pemakaian <= 24) {
+        totalBiaya = 455000; /* kl kurang / sama dgn 24jam */
     } else {
-        totalBiaya = HARGA_24_JAM_PERTAMA;
-        int sisaJam = jam - 24;
-
+        totalBiaya = 455000;
+        int sisaJam = pemakaian - 24;
         if (sisaJam > 0) {
-            if (sisaJam <= 2) {
-                totalBiaya += HARGA_2_JAM_KEDUA;
-            } else {
-                totalBiaya += HARGA_2_JAM_KEDUA;
-                sisaJam -= 2;
-
-                if (sisaJam <= 2) {
-                    totalBiaya += HARGA_2_JAM_KETIGA;
-                } else {
-                    totalBiaya += HARGA_2_JAM_KETIGA;
-                    sisaJam -= 2;
-                    
-                    totalBiaya += (sisaJam / 2) * HARGA_2_JAM_SELANJUTNYA;
-                    if (sisaJam % 2 != 0) {
-                        totalBiaya += (HARGA_2_JAM_SELANJUTNYA / 2);
-                    }
-                }
+            totalBiaya += 55000; /* 2 jam kedua */
+            sisaJam -= 2;
+        }
+        if (sisaJam > 0) {
+            totalBiaya += 75000; /* 2 jam ketiga */
+            sisaJam -= 2;
+        }
+        
+		/* 2 jam seterusnya */
+        if (sisaJam > 0) {
+            int total_2_jam_lebih = sisaJam / 2; 
+            if (sisaJam % 2 != 0) {
+                total_2_jam_lebih++; /* kl sisa 2 jam */
             }
+            totalBiaya += total_2_jam_lebih * 95000; /* dikali 2 jam seterusnya * 95k */
         }
     }
 
-    if (jam > 35) {
-        totalBiaya -= totalBiaya * DISKON_35_JAM;
+    /* diskon */
+	if (pemakaian > 35) {
+	    totalBiaya = totalBiaya * 0.85; /* potongan 15% dr total */
+	}
+    if (pemakaian > 48) {
+        totalBiaya -= 455000 * 0.10; /* Potongan 10% dr 24jam */
     }
 
-    if (jam > 48) {
-        totalBiaya -= HARGA_24_JAM_PERTAMA * DISKON_48_JAM;
-    }
-
-    // Menampilkan hasil
-    cout << "Total biaya sewa: Rp" << totalBiaya << endl;
+    cout << "Total Biaya: IDR" << fixed << setprecision(0) << totalBiaya << endl;
 
     return 0;
 }
